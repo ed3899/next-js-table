20318423d4b956407e22f3247f4f2ce1455ba5d5
eduardo-casanova <ed.wacc1995@gmail.com>
2023-07-03 20:31:52 +0000

    task complete

--- src/pages/table.tsx
+++ src/pages/table.tsx
@@ -59,29 +59,18 @@ const TablePage: React.FC<TableProps> = ({ users, error }) => {
       <Table.Header>
         <Table.Column>NAME</Table.Column>
         <Table.Column>EMAIL</Table.Column>
-        <Table.Column>Gender</Table.Column>
+        <Table.Column>GENDER</Table.Column>
+        <Table.Column>STATUS</Table.Column>
       </Table.Header>
       <Table.Body>
-        <Table.Row key="1">
-          <Table.Cell>Tony Reichert</Table.Cell>
-          <Table.Cell>CEO</Table.Cell>
-          <Table.Cell>Active</Table.Cell>
-        </Table.Row>
-        <Table.Row key="2">
-          <Table.Cell>Zoey Lang</Table.Cell>
-          <Table.Cell>Technical Lead</Table.Cell>
-          <Table.Cell>Paused</Table.Cell>
-        </Table.Row>
-        <Table.Row key="3">
-          <Table.Cell>Jane Fisher</Table.Cell>
-          <Table.Cell>Senior Developer</Table.Cell>
-          <Table.Cell>Active</Table.Cell>
-        </Table.Row>
-        <Table.Row key="4">
-          <Table.Cell>William Howard</Table.Cell>
-          <Table.Cell>Community Manager</Table.Cell>
-          <Table.Cell>Vacation</Table.Cell>
-        </Table.Row>
+      {users.map((user, index) => (
+    <Table.Row key={index}>
+      <Table.Cell>{user.name} </Table.Cell>
+      <Table.Cell>{user.email}</Table.Cell>
+      <Table.Cell>{user.gender}</Table.Cell>
+      <Table.Cell>{user.status}</Table.Cell>
+    </Table.Row>
+  ))}
       </Table.Body>
     </Table>
   )
